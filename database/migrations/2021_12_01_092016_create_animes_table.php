8a0843221da24335772722243ba3ef5ae68a750e
listentotheconscience <iseethatshitlikethesharingan@gmail.com>
2021-12-01 15:32:50 +0300

    Initial commit

---
+++ database/migrations/2021_12_01_092016_create_animes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnimesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('episodes');
+            $table->text('status');
+            $table->json('licensors');
+            $table->json('genres');
+            $table->decimal('rating');
+            $table->text('season');
+            $table->text('type');
+            $table->json('produsers');
+            $table->json('studios');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('animes');
+    }
+}
